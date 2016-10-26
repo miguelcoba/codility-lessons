b565bddc681a5450a975393d005394801a82e94e
Miguel Coba <miguel.coba@gmail.com>
2016-10-26 05:19:30 +0000

    New version, O(n)

--- lesson04/MissingInteger.js
+++ lesson04/MissingInteger.js
@@ -1,17 +1,22 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
 function solution(A) {
-    // write your code in JavaScript (Node.js 0.12)
+    // write your code in JavaScript (Node.js 6.4.0)
     
-    var sorted = A.sort(function(a, b) {
-        return a - b;
-    });
-    
-    var n = sorted.length,
-        currentMin = 1;
-    for (var i=0; i<n; i++) {
-        if (n > 0 && sorted[i] == currentMin) {
-            currentMin++;
+    N = 100001
+    var bitmap = Array(N).fill(false);
+    for (var i = 0; i < A.length; i ++) {
+        if (A[i] > 0 && A[i] <= N) {
+            bitmap[A[i]] = true;
         }
     }
     
-    return currentMin;
+    for(var j = 1; j <= N; j++) {
+        if (!bitmap[j]) {
+            return j;
+        }
+    }
+    
+    return N + 1;
 }
